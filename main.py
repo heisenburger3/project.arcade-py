b8e4eba7efe40634c8af82a373743cb28720c1e5
Rinat <rinat.rinat.1302@gmail.com>
2025-12-23 17:13:23 +0300

    Готовый второй уровень, звуковое сопровождение, были добавлены комментарии в коде первого уровня для упрощения понимания кода (для второго дублировать не стал)

--- main.py
+++ main.py
@@ -3,28 +3,22 @@ import arcade
 
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 1000
-SCREEN_TITLE = "Worm Dinner"
+SCREEN_TITLE = "Worm's Dinner"
 TILE_SCALING = 3.15
 TILE_SIZE = 16
-SPEED = 0
 CAMERA_LERP = 0.1
-# Мёртвая зона камеры
 DEAD_ZONE_W = int(SCREEN_WIDTH * 0.3)
 DEAD_ZONE_H = int(SCREEN_HEIGHT * 0.4)
 
 
-class StartView(arcade.View): # Главное меню игры
+class StartView(arcade.View):  # Главное меню игры
     def on_show(self):
-        """Настройка начального экрана"""
         arcade.set_background_color(arcade.color.BLACK)
 
     def on_draw(self):
-        """Отрисовка начального экрана"""
         self.clear()
-
-        # Батч для текста
         self.batch = Batch()
-        start_text = arcade.Text("Worm Dinner", self.window.width / 2, self.window.height / 2,
+        start_text = arcade.Text("Worm's Dinner", self.window.width / 2, self.window.height / 2,
                                  arcade.color.WHITE, font_size=50, anchor_x="center", batch=self.batch)
         any_key_text = arcade.Text("Any key to start",
                                    self.window.width / 2, self.window.height / 2 - 75,
@@ -32,7 +26,6 @@ class StartView(arcade.View): # Главное меню игры
         self.batch.draw()
 
     def on_key_press(self, key, modifiers):
-        """Начало игры при нажатии клавиши"""
         level_view = LevelFirst()
         level_view.setup()
         self.window.show_view(level_view)
@@ -43,7 +36,6 @@ class LevelFirst(arcade.View):
         super().__init__()
         arcade.set_background_color(arcade.color.GRAY)
 
-        # Камеры: мир и GUI
         self.world_camera = arcade.camera.Camera2D()
         self.gui_camera = arcade.camera.Camera2D()
 
@@ -56,22 +48,27 @@ class LevelFirst(arcade.View):
         )
 
     def setup(self):
-        """Настраиваем игру здесь. Вызывается при старте и при рестарте"""
         # Списки спрайтов, спрайт карты и игрок
         self.player_list = arcade.SpriteList()
         self.apple_list = arcade.SpriteList()
         tile_map = arcade.load_tilemap('assets/first_level.tmx', scaling=TILE_SCALING)
-        self.player_sprite = arcade.Sprite(":resources:images/enemies/wormGreen.png",
-                                           0.38)
-        self.total_time = 0.0
+        self.player_sprite = arcade.Sprite(":resources:images/enemies/wormGreen.png",0.38)
         self.player_sprite.center_x = 48
         self.player_sprite.center_y = 50
         self.player_list.append(self.player_sprite)
 
+        # Таймер
+        self.total_time = 0.0
+
+        # Батч
         self.batch = Batch()
 
         # Подсёт очков
         self.score = 0
+
+        # Музыкальное сопровождение
+        audio = arcade.load_sound('assets/game_music.mp3', False)
+        arcade.play_sound(audio, 1.0, 0, True)
 
         # С помощью этого, наш червяк сможет адекватно передвигаться с зажатыми клавишами
         self.up_pressed = False
@@ -84,6 +81,7 @@ class LevelFirst(arcade.View):
         self.base_list = tile_map.sprite_lists['base']
         self.path_list = tile_map.sprite_lists['path']
 
+        # Физика
         self.physics_engine = arcade.PhysicsEngineSimple(
             self.player_sprite, self.collision_list)
 
@@ -94,11 +92,9 @@ class LevelFirst(arcade.View):
         self.apple_list.append(apple)
 
         self.world_width = SCREEN_WIDTH
-
         self.world_height = SCREEN_HEIGHT
 
     def on_draw(self):
-        """Отрисовка экрана"""
         self.clear()
 
         # Отрисовка карты и камеры
@@ -113,15 +109,23 @@ class LevelFirst(arcade.View):
         self.batch.draw()
 
     def on_update(self, delta_time: float):
+        # Физика
         self.physics_engine.update()
+
+        # Тряска камеры при подборе яблока
         self.camera_shake.update(delta_time)
+
+        # Механика яблока
         apples_hit_list = arcade.check_for_collision_with_list(self.player_sprite, self.apple_list)
 
         for apple in apples_hit_list:
             self.camera_shake.start()
             self.score += 100
+            apple_collect = arcade.load_sound("assets/apple_collect.mp3", False)
+            arcade.play_sound(apple_collect, 5.0, 0, False)
             apple.remove_from_sprite_lists()
 
+        # Подсчёт очков (текст)
         self.score_text = arcade.Text(
             f"Счет: {self.score}", 10, SCREEN_HEIGHT - 30,
             arcade.color.WHITE, 20, batch=self.batch)
@@ -144,16 +148,14 @@ class LevelFirst(arcade.View):
         self.player_sprite.change_x = change_x
         self.player_sprite.change_y = change_y
 
+        # Камера
         position = (
             self.player_sprite.center_x,
             self.player_sprite.center_y
         )
-        self.world_camera.position = arcade.math.lerp_2d(  # Изменяем позицию камеры
-            self.world_camera.position,
-            position,
-            CAMERA_LERP,
-        )# Плавность следования камеры
+        self.world_camera.position = arcade.math.lerp_2d(self.world_camera.position, position, CAMERA_LERP,)
 
+        # Таймер
         self.total_time += delta_time
         self.fonts = arcade.Text(
             f"Время: {self.total_time:.2f} сек",
@@ -164,6 +166,7 @@ class LevelFirst(arcade.View):
             batch=self.batch
         )
 
+        # Переход на следующий уровень
         if len(self.apple_list) == 0:
             level_second = LevelSecond()
             level_second.setup()
@@ -195,7 +198,6 @@ class LevelSecond(arcade.View):
         super().__init__()
         arcade.set_background_color(arcade.color.GRAY)
 
-        # Камеры: мир и GUI
         self.world_camera = arcade.camera.Camera2D()
         self.gui_camera = arcade.camera.Camera2D()
 
@@ -217,7 +219,7 @@ class LevelSecond(arcade.View):
         # Списки спрайтов, спрайт карты и игрок
         self.player_list = arcade.SpriteList()
         self.apple_list = arcade.SpriteList()
-        tile_map = arcade.load_tilemap('assets/first_level.tmx', scaling=TILE_SCALING)
+        tile_map = arcade.load_tilemap('assets/second_level.tmx', scaling=TILE_SCALING)
         self.player_sprite = arcade.Sprite(":resources:images/enemies/wormGreen.png",
                                            0.38)
         self.player_sprite.center_x = 48
@@ -249,7 +251,6 @@ class LevelSecond(arcade.View):
         self.world_height = SCREEN_HEIGHT
 
     def on_draw(self):
-        """Отрисовка экрана"""
         self.clear()
 
         # Отрисовка карты и камеры
@@ -271,6 +272,8 @@ class LevelSecond(arcade.View):
         for apple in apples_hit_list:
             self.camera_shake.start()
             self.score += 100
+            apple_collect = arcade.load_sound("assets/apple_collect.mp3", False)
+            arcade.play_sound(apple_collect, 5.0, 0, False)
             apple.remove_from_sprite_lists()
 
         self.score_text = arcade.Text(
@@ -335,7 +338,6 @@ class LevelSecond(arcade.View):
 
 
 def main():
-    """Главная функция"""
     window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
     start_view = StartView()
     window.show_view(start_view)
