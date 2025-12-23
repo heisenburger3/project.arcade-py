82e7582d038bc7e5a006a8ce8925c8edb687e7e7
Rinat <rinat.rinat.1302@gmail.com>
2025-12-23 16:28:28 +0300

    Добавил папку assets и содержимое

---
+++ assets/colored_sheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="colored_sheet" tilewidth="16" tileheight="16" spacing="1" tilecount="1078" columns="49">
+ <image source="colored.png" width="832" height="373"/>
+</tileset>
